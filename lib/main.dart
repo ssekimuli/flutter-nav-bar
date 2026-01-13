0c0676c19170436bc9431f4e7b60d9a344bd9809
ssekimuli <ssekimuliandrew321@gmail.com>
2026-01-13 12:40:04 +0300

    add responsive bar

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Responsive navigation demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  static const double breakpoint = 600;
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) { 
+      if (constraints.maxWidth >= breakpoint) {
+         return Scaffold(
+            appBar: AppBar(title: Text('Large Screen View')),
+            body: Row(
+              children: [
+                SizedBox(
+                  width: 240,
+                  child: AppMenu(),
+                ),
+                Expanded(
+                  child: Center(child: Text('Main Content Area')),
+                ),
+              ],
+            ),
+          );
+      } else {
+        return Scaffold(
+            appBar: AppBar(
+              title: const Text('Small Screen View'),
+              // The leading hamburger icon appears automatically if a Drawer is provided
+            ),
+            drawer: const Drawer(
+              child: AppMenu(),
+            ),
+            body: const Center(child: Text('Main Content Area')),
+          );
+      }
+    });
+  }
+}
+
+class AppMenu extends StatelessWidget {
+  const AppMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: [
+        // const DrawerHeader(
+        //   decoration: BoxDecoration(
+        //     color: Colors.blue,
+        //   ),
+        //   child: Text('Navigation Menu'),
+        // ),
+        ListTile(
+          title: const Text('Home'),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          title: const Text('Profile'),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          title: const Text('Settings'),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+  }
+}
